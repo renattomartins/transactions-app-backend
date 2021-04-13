d1ef6ae35e5ba7af421dbbe4fec7456e9d04e470
Renato Martins <renatto.martins@gmail.com>
2021-04-13 07:32:25 -0300

    Adjusts Jest sample tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,12 @@
-it("Testing to see if Jest works", () => {
-  expect(1).toBe(1);
+describe("Jest sample", () => {
+  it("Testing to see if Jest works", () => {
+    expect(1).toBe(1);
+  });
+
+  it("Async test", async (done) => {
+    // Do your async tests here
+
+    done();
+  });
 });
+
