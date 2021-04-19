5c70e64a650c9a977f63c14f68cd6672b38e3a3a
Renato Martins <renatto.martins@gmail.com>
2021-04-19 08:10:38 -0300

    Adjusts message in start file

--- start.js
+++ start.js
@@ -2,5 +2,5 @@ const app = require("./src/index.js");
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Transactions App listening at http://localhost:${port}`);
+  console.log(`Transactions API listening at http://localhost:${port}`);
 });
