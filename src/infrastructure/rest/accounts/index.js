ea5d92aedbb0adea762d72649d8b6b9870b6b551
Renato Martins <renatto.martins@gmail.com>
2021-04-22 08:02:15 -0300

    Includes account endpoints file into correct clean architecture folder

---
+++ src/infrastructure/rest/accounts/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const router = express.Router();
+const routes = require("./accounts.js");
+
+module.exports = routes(router);
---
+++ test/infrastructure/rest/accounts.test.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const supertest = require("supertest");
+const accountsRouter = require("../../../src/infrastructure/rest/accounts/accounts.js");
+
+const prepareTestScenario = () => {
+  const app = express();
+  const router = express.Router();
+  const routes = accountsRouter(router);
+  app.use(routes);
+
+  return supertest(app);
+};
+
+describe("Accounts end points", () => {
+  it("Should return a valid response with an collection of account resources", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.get("/accounts");
+
+    expect(res.status).toBe(200);
+    expect(res.headers.hasOwnProperty("content-type")).toBe(true);
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+});
