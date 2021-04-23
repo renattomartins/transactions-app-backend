ea1ab0f3ff91a53ed5b00a6599d8ab94aa636d85
Renato Martins <renatto.martins@gmail.com>
2021-04-23 07:47:38 -0300

    Improves unit test coverage

--- test/infrastructure/rest/accounts.test.js
+++ test/infrastructure/rest/accounts.test.js
@@ -12,7 +12,7 @@ const prepareTestScenario = () => {
 };
 
 describe("Accounts end points", () => {
-  it("Should return a valid response with an collection of account resources", async (done) => {
+  it("GET /accounts should return a valid response with an collection of account resources", async (done) => {
     const request = prepareTestScenario();
     const res = await request.get("/accounts");
 
@@ -20,6 +20,55 @@ describe("Accounts end points", () => {
     expect(res.headers.hasOwnProperty("content-type")).toBe(true);
     expect(res.headers.hasOwnProperty("x-total-count")).toBe(true);
     expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+
+  it("GET /accounts/:id should return a valid response with an account resource", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.get("/accounts/3541");
+
+    expect(res.status).toBe(200);
+    expect(res.headers.hasOwnProperty("content-type")).toBe(true);
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+
+  it("POST /accounts/:id should return a valid response with a new account resource", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.post("/accounts");
+
+    expect(res.status).toBe(201);
+    expect(res.headers.hasOwnProperty("content-type")).toBe(true);
+    expect(res.headers.hasOwnProperty("location")).toBe(true);
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+
+  it("PUT /accounts/:id should return a valid response with a changed account resource", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.put("/accounts/3544");
+
+    expect(res.status).toBe(200);
+    expect(res.headers.hasOwnProperty("content-type")).toBe(true);
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+
+  it("PATCH /accounts/:id should return a valid response with a parcially changed account resource", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.patch("/accounts/3544");
+
+    expect(res.status).toBe(200);
+    expect(res.headers.hasOwnProperty("content-type")).toBe(true);
+    expect(res.headers["content-type"]).toMatch(/application\/json/);
+    done();
+  });
+
+  it("DELETE /accounts/:id should return a valid response to represent a deleted account resource", async (done) => {
+    const request = prepareTestScenario();
+    const res = await request.delete("/accounts/3544");
+
+    expect(res.status).toBe(204);
     done();
   });
 });
