a1dd4d1b7d7b0799f0b624c829e972a00bac6c20
Renato Martins <renatto.martins@gmail.com>
2021-05-10 15:30:57 -0300

    Makes a refactoring to simplify calls to routes modules from application entry point

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const restRoutes = require('./infrastructure/rest');
 
 const app = express();
+const router = express.Router();
+
+const healthCheckRoutes = require('./infrastructure/rest/healthCheck.js');
+const initialRoutes = require('./infrastructure/rest/initialRoute.js');
+const accountsRoutes = require('./infrastructure/rest/accounts.js');
+const transactionsRoutes = require('./infrastructure/rest/transactions.js');
+
+healthCheckRoutes(router);
+initialRoutes(router);
+accountsRoutes(router);
+transactionsRoutes(router);
 
 app.use(express.json());
-app.use(restRoutes);
+app.use(router);
 
 module.exports = app;
