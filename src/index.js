f7d015047a61184e84b769109343f580b2fd55ed
Renato Martins <renatto.martins@gmail.com>
2021-05-12 08:01:41 -0300

    Makes function return of routes more explicit for better readability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
-
-const app = express();
-const router = express.Router();
-
 const healthCheckRoutes = require('./infrastructure/rest/healthCheck.js');
 const initialRoutes = require('./infrastructure/rest/initialRoute.js');
 const accountsRoutes = require('./infrastructure/rest/accounts.js');
 const transactionsRoutes = require('./infrastructure/rest/transactions.js');
 
-healthCheckRoutes(router);
-initialRoutes(router);
-accountsRoutes(router);
-transactionsRoutes(router);
+const app = express();
+let router = express.Router();
+
+router = healthCheckRoutes(router);
+router = initialRoutes(router);
+router = accountsRoutes(router);
+router = transactionsRoutes(router);
 
 app.use(express.json());
 app.use(router);
