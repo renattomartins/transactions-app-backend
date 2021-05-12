023378c6edbaabe2f37c0156850f0ec7e52cb54d
Renato Martins <renatto.martins@gmail.com>
2021-05-12 08:06:04 -0300

    Improves intial route response body

--- src/infrastructure/rest/initialRoute.js
+++ src/infrastructure/rest/initialRoute.js
@@ -2,7 +2,8 @@ const routes = (router) => {
   router.get('/', (req, res) => {
     res.json({
       code: 0,
-      message: 'success',
+      message: 'Welcome to Transaction API',
+      version: '1.0.0',
       resources: {
         account_url: `${req.protocol}://${req.get('host')}/accounts`,
       },
