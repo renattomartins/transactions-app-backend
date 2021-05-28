af7ed0bb1eb7eb7cc5d6433be03eb2db3f931140
Renato Martins <renatto.martins@gmail.com>
2021-05-28 08:54:54 -0300

    Configures Jest to execute gracefully with Sequelize lazy loading of encodings

--- src/core/accounts/Account.js
+++ src/core/accounts/Account.js
@@ -15,10 +15,10 @@ const testConnection = async (sequelize) => {
   try {
     await sequelize.authenticate();
     // eslint-disable-next-line no-console
-    console.log('Connection has been established successfully.');
+    // console.log('Connection has been established successfully.');
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.log('Unable to connect to the database', error);
+    // console.log('Unable to connect to the database', error);
   }
 };
 
