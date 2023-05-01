07ae199a31da52d000c8694f47cbc5cf08b8f061
Renato Martins <renatto.martins@gmail.com>
2023-05-01 12:20:23 -0300

    Moves mock data from accounts controllers to test file and creates an anemic controllers recovery accounts and create new accounts

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,107 +1,40 @@
-exports.getAccounts = (req, res) => {
-  res.set('X-Total-Count', 3);
-  res.json([
-    {
-      id: 3541,
-      name: 'Bradesco, C/C',
-      icon: 'icon-bradesco',
-      description: '',
-      type: 1,
-      current_amount: 2349.89,
-      activated: true,
-      created: '2012-06-13 19:05:15',
-      modified: '2015-01-16 18:49:26',
-      account_url: `${req.protocol}://${req.get('host')}/accounts/3541`,
-    },
-    {
-      id: 3542,
-      name: 'Bradesco, C/P',
-      icon: 'icon-bradesco',
-      description: '',
-      type: 2,
-      current_amount: 1804.32,
-      activated: true,
-      created: '2012-06-13 19:08:04',
-      modified: '2015-01-16 18:49:29',
-      account_url: `${req.protocol}://${req.get('host')}/accounts/3542`,
-    },
-    {
-      id: 3543,
-      name: 'Carteira',
-      icon: 'icon-wallet',
-      description: '',
-      type: 5,
-      current_amount: 34.0,
-      created: '2012-06-13 19:08:08',
-      modified: '2014-03-15 11:25:31',
-      account_url: `${req.protocol}://${req.get('host')}/accounts/3543`,
-    },
-  ]);
+const User = require('./../models/user');
+
+exports.getAccounts = async (req, res) => {
+  const theOne = await User.findByPk(1);
+  const accounts = await theOne.getAccounts();
+  res.set('X-Total-Count', accounts.length);
+  res.json(accounts);
 };
 
-exports.createAccount = (req, res) => {
-  const location = `${req.protocol}://${req.get('host')}/accounts/3544`;
+exports.createAccount = async (req, res) => {
+  const theOne = await User.findByPk(1);
+  const account = await theOne.createAccount({
+    name: req.body.name,
+    icon: req.body.icon,
+    description: req.body.description,
+    type: req.body.type,
+    currentBalance: req.body.currentBalance,
+    activated: req.body.activated,
+  });
+  const location = `${req.protocol}://${req.get('host')}/accounts/${account.id}`;
 
   res.set('Location', location);
-  res.status(201).json({
-    id: 3544,
-    name: 'Banco Inter, C/C',
-    icon: 'icon-inter',
-    description: '',
-    type: 1,
-    current_amount: 0.0,
-    activated: true,
-    created: '2021-04-09 08:13:15',
-    modified: '2021-04-09 08:13:15',
-    account_url: location,
-  });
+  res.status(201).json(account);
 };
 
 exports.getAccount = (req, res) => {
-  res.json({
-    id: 3541,
-    name: 'Bradesco, C/C',
-    icon: 'icon-bradesco',
-    description: '',
-    type: 1,
-    current_amount: 2349.89,
-    activated: true,
-    created: '2012-06-13 19:05:15',
-    modified: '2015-01-16 18:49:26',
-    account_url: `${req.protocol}://${req.get('host')}/accounts/3541`,
-  });
+
 };
 
 exports.updateAccount = (req, res) => {
-  res.json({
-    id: 3544,
-    name: 'Banco Intermedium, C/C',
-    icon: 'icon-inter',
-    description: 'Banco para investimentos e reserva de emergência',
-    type: 1,
-    current_amount: 10000.0,
-    activated: true,
-    created: '2021-04-09 08:13:15',
-    modified: '2021-04-09 08:37:36',
-    account_url: `${req.protocol}://${req.get('host')}/accounts/3544`,
-  });
+  
 };
 
 exports.partiallyUpdateAccount = (req, res) => {
-  res.json({
-    id: 3544,
-    name: 'Banco Intermedium, C/C',
-    icon: 'icon-inter',
-    description: 'Banco para investimentos e reserva de emergência',
-    type: 1,
-    current_amount: 10000.0,
-    activated: false,
-    created: '2021-04-09 08:13:15',
-    modified: '2021-04-09 08:37:36',
-    account_url: `${req.protocol}://${req.get('host')}/accounts/3544`,
-  });
+  
 };
 
 exports.deleteAccount = (req, res) => {
-  res.sendStatus(204);
+  
 };
---
+++ test/unit/controllers/accounts.test.js
@@ -0,0 +1,116 @@
+const Account = require('../../../src/models/account');
+const accountsController = require('../../../src/controllers/accounts');
+
+describe.skip('Users controllers', () => {
+  it('Should have unit test to getAccounts endpoint', () => {
+    // @todo
+    // res.set('X-Total-Count', 3);
+    // res.json([
+    //   {
+    //     id: 3541,
+    //     name: 'Bradesco, C/C',
+    //     icon: 'icon-bradesco',
+    //     description: '',
+    //     type: 1,
+    //     current_amount: 2349.89,
+    //     activated: true,
+    //     created: '2012-06-13 19:05:15',
+    //     modified: '2015-01-16 18:49:26',
+    //     account_url: `${req.protocol}://${req.get('host')}/accounts/3541`,
+    //   },
+    //   {
+    //     id: 3542,
+    //     name: 'Bradesco, C/P',
+    //     icon: 'icon-bradesco',
+    //     description: '',
+    //     type: 2,
+    //     current_amount: 1804.32,
+    //     activated: true,
+    //     created: '2012-06-13 19:08:04',
+    //     modified: '2015-01-16 18:49:29',
+    //     account_url: `${req.protocol}://${req.get('host')}/accounts/3542`,
+    //   },
+    //   {
+    //     id: 3543,
+    //     name: 'Carteira',
+    //     icon: 'icon-wallet',
+    //     description: '',
+    //     type: 5,
+    //     current_amount: 34.0,
+    //     created: '2012-06-13 19:08:08',
+    //     modified: '2014-03-15 11:25:31',
+    //     account_url: `${req.protocol}://${req.get('host')}/accounts/3543`,
+    //   },
+    // ]);
+  });
+
+  it('Should have unit test to createAccount endpoint', () => {
+    // @todo
+    // res.set('Location', location);
+    // res.status(201).json({
+    //   id: 3544,
+    //   name: 'Banco Inter, C/C',
+    //   icon: 'icon-inter',
+    //   description: '',
+    //   type: 1,
+    //   current_amount: 0.0,
+    //   activated: true,
+    //   created: '2021-04-09 08:13:15',
+    //   modified: '2021-04-09 08:13:15',
+    //   account_url: location,
+    // });
+  });
+
+  it('Should have unit test to getAccount endpoint', () => {
+    // @todo
+    // res.json({
+    //   id: 3541,
+    //   name: 'Bradesco, C/C',
+    //   icon: 'icon-bradesco',
+    //   description: '',
+    //   type: 1,
+    //   current_amount: 2349.89,
+    //   activated: true,
+    //   created: '2012-06-13 19:05:15',
+    //   modified: '2015-01-16 18:49:26',
+    //   account_url: `${req.protocol}://${req.get('host')}/accounts/3541`,
+    // });
+  });
+
+  it('Should have unit test to updateAccount endpoint', () => {
+    // @todo
+    // res.json({
+    //   id: 3544,
+    //   name: 'Banco Intermedium, C/C',
+    //   icon: 'icon-inter',
+    //   description: 'Banco para investimentos e reserva de emergência',
+    //   type: 1,
+    //   current_amount: 10000.0,
+    //   activated: true,
+    //   created: '2021-04-09 08:13:15',
+    //   modified: '2021-04-09 08:37:36',
+    //   account_url: `${req.protocol}://${req.get('host')}/accounts/3544`,
+    // });
+  });
+
+  it('Should have unit test to partiallyUpdateAccount endpoint', () => {
+    // @todo
+    // res.json({
+    //   id: 3544,
+    //   name: 'Banco Intermedium, C/C',
+    //   icon: 'icon-inter',
+    //   description: 'Banco para investimentos e reserva de emergência',
+    //   type: 1,
+    //   current_amount: 10000.0,
+    //   activated: false,
+    //   created: '2021-04-09 08:13:15',
+    //   modified: '2021-04-09 08:37:36',
+    //   account_url: `${req.protocol}://${req.get('host')}/accounts/3544`,
+    // });
+  });
+
+  it('Should have unit test to deleteAccount endpoint', () => {
+    // @todo
+    // res.sendStatus(204);
+  });
+});
