7ecefae6f83832602155b618fa35bd4b906af3db
Renato Martins <renatto.martins@gmail.com>
2023-05-04 16:45:19 -0300

    Includes error handling in Accounts Controller

--- src/controllers/accounts.js
+++ src/controllers/accounts.js
@@ -1,79 +1,136 @@
 const User = require('./../models/user');
 
 exports.getAccounts = async (req, res) => {
-  const theOne = await User.findByPk(1);
-  const accounts = await theOne.getAccounts();
-  res.set('X-Total-Count', accounts.length);
-  res.json(accounts);
+  try {
+    const theOne = await User.findByPk(1);
+    const accounts = await theOne.getAccounts();
+
+    res.set('X-Total-Count', accounts.length);
+    res.json(accounts);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
+
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
 
 exports.createAccount = async (req, res) => {
-  const theOne = await User.findByPk(1);
-  const account = await theOne.createAccount({
-    name: req.body.name,
-    icon: req.body.icon,
-    description: req.body.description,
-    type: req.body.type,
-    initialBalance: req.body.initialBalance,
-    activated: req.body.activated,
-  });
-  const location = `${req.protocol}://${req.get('host')}/accounts/${account.id}`;
+  const { name, icon, description, type, initialBalance, activated } = req.body;
+  try {
+    const theOne = await User.findByPk(1);
+    const account = await theOne.createAccount({
+      name: name,
+      icon: icon,
+      description: description,
+      type: type,
+      initialBalance: initialBalance,
+      activated: activated,
+    });
+    const location = `${req.protocol}://${req.get('host')}/accounts/${account.id}`;
 
-  res.set('Location', location);
-  res.status(201).json(account);
+    res.set('Location', location);
+    res.status(201).json(account);
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
+
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
 
 exports.getAccount = async (req, res) => {
   const accountId = req.params.accountId;
-  const theOne = await User.findByPk(1);
-  const account = await theOne.getAccounts({ where: { id: accountId } });
 
-  if (account[0] != undefined) res.json(account[0]);
-  else res.status(404).json({ code: 404, message: 'Not found' });
+  try {
+    const theOne = await User.findByPk(1);
+    const account = await theOne.getAccounts({ where: { id: accountId } });
+
+    if (account[0] != undefined) res.json(account[0]);
+    else res.status(404).json({ code: 404, message: 'Not found' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
+
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
 
 exports.updateAccount = async (req, res) => {
   const accountId = req.params.accountId;
-  const theOne = await User.findByPk(1);
-  const accounts = await theOne.getAccounts({ where: { id: accountId } });
+  const { name, icon, description, type, initialBalance, activated } = req.body;
 
-  if (accounts[0] != undefined) {
-    const account = accounts[0];
+  try {
+    const theOne = await User.findByPk(1);
+    const accounts = await theOne.getAccounts({ where: { id: accountId } });
 
-    account.name = req.body.name;
-    account.icon = req.body.icon;
-    account.description = req.body.description;
-    account.type = req.body.type;
-    account.initialBalance = req.body.initialBalance;
-    account.activated = req.body.activated;
+    if (accounts[0] != undefined) {
+      const account = accounts[0];
 
-    try {
+      account.name = name;
+      account.icon = icon;
+      account.description = description;
+      account.type = type;
+      account.initialBalance = initialBalance;
+      account.activated = activated;
+
       const updatedAccount = await account.save();
-
       res.json(updatedAccount);
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.log(`Error! ${e}`);
+    } else res.status(404).json({ code: 404, message: 'Not found' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
 
-      res.status(500).json({ code: 500, message: 'Internal Server Error' });
-    }
-  } else res.status(404).json({ code: 404, message: 'Not found' });
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
 
 exports.partiallyUpdateAccount = async (req, res) => {
   const accountId = req.params.accountId;
-  const theOne = await User.findByPk(1);
-  const account = await theOne.getAccounts({ where: { id: accountId } });
+  const { name, icon, description, type, initialBalance, activated } = req.body;
 
-  if (account[0] != undefined) res.json(account[0]);
-  else res.status(404).json({ code: 404, message: 'Not found' });
+  try {
+    const theOne = await User.findByPk(1);
+    const accounts = await theOne.getAccounts({ where: { id: accountId } });
+
+    if (accounts[0] != undefined) {
+      const account = accounts[0];
+
+      account.name = name ? name : account.name;
+      account.icon = icon ? icon : account.icon;
+      account.description = description ? description : account.description;
+      account.type = type ? type : account.type;
+      account.initialBalance = initialBalance ? initialBalance : account.initialBalance;
+      account.activated = activated != undefined ? activated : account.activated;
+
+      const updatedAccount = await account.save();
+      res.json(updatedAccount);
+    } else res.status(404).json({ code: 404, message: 'Not found' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
+
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
 
 exports.deleteAccount = async (req, res) => {
   const accountId = req.params.accountId;
-  const theOne = await User.findByPk(1);
-  const account = await theOne.getAccounts({ where: { id: accountId } });
 
-  if (account[0] != undefined) res.json(account[0]);
-  else res.status(404).json({ code: 404, message: 'Not found' });
+  try {
+    const theOne = await User.findByPk(1);
+    const accounts = await theOne.getAccounts({ where: { id: accountId } });
+
+    if (accounts[0] != undefined) {
+      const account = accounts[0];
+
+      await account.destroy();
+      res.status(204).send();
+    } else res.status(404).json({ code: 404, message: 'Not found' });
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(`Error! ${e}`);
+
+    res.status(500).json({ code: 500, message: 'Internal Server Error' });
+  }
 };
