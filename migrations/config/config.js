1c98aea74f4c1fd026de94bd0899eafabe8b1946
Renato Martins <renatto.martins@gmail.com>
2023-05-07 13:32:30 -0300

    Includes proper migrations configs for the project

---
+++ migrations/config/config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_MIGRATION_USER,
+    password: process.env.DB_MIGRATION_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_MIGRATION_USER,
+    password: process.env.DB_MIGRATION_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_MIGRATION_USER,
+    password: process.env.DB_MIGRATION_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+};
