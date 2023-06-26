ce28980f60b0dc32a4e508baeaac6b78533d503a
Renato Martins <renatto.martins@gmail.com>
2023-06-26 08:20:22 -0300

    Adds isAuth middleware to verify user authentication

--- src/routes/initial.js
+++ src/routes/initial.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const isAuth = require('../middlewares/is-auth');
 
 const initialController = require('../controllers/initial');
 
 const router = express.Router();
 
-router.get('/', initialController.getInitial);
+router.get('/', isAuth, initialController.getInitial);
 
 module.exports = router;
--- test/integration/routes/initial.test.js
+++ test/integration/routes/initial.test.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const supertest = require('supertest');
+const jwt = require('jsonwebtoken');
 const initialRouter = require('../../../src/routes/initial.js');
 
 const prepareTestScenario = () => {
@@ -12,7 +13,9 @@ const prepareTestScenario = () => {
 describe('Proper functioning of API entry point route', () => {
   it('GET /', async (done) => {
     const request = prepareTestScenario();
-    const res = await request.get('/');
+    jwt.verify = jest.fn().mockReturnValueOnce({ user: 123, token: 'abc' });
+
+    const res = await request.get('/').set({ Authorization: 'Bearer abc' });
 
     expect(res.status).toBe(200);
     expect(Object.prototype.hasOwnProperty.call(res.headers, 'content-type')).toBe(true);
