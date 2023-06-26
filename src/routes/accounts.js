98f50ef4b771a68398efa8521d73acf172d4cd54
Renato Martins <renatto.martins@gmail.com>
2023-06-26 08:46:12 -0300

    Adds auth verification in all accounts routes

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,14 +1,15 @@
 const express = require('express');
 
+const isAuth = require('../middlewares/is-auth');
 const accountsController = require('../controllers/accounts');
 
 const router = express.Router();
 
-router.get('/accounts', accountsController.getAccounts);
-router.post('/accounts', accountsController.createAccount);
-router.get('/accounts/:accountId', accountsController.getAccount);
-router.put('/accounts/:accountId', accountsController.updateAccount);
-router.patch('/accounts/:accountId', accountsController.partiallyUpdateAccount);
-router.delete('/accounts/:accountId', accountsController.deleteAccount);
+router.get('/accounts', isAuth, accountsController.getAccounts);
+router.post('/accounts', isAuth, accountsController.createAccount);
+router.get('/accounts/:accountId', isAuth, accountsController.getAccount);
+router.put('/accounts/:accountId', isAuth, accountsController.updateAccount);
+router.patch('/accounts/:accountId', isAuth, accountsController.partiallyUpdateAccount);
+router.delete('/accounts/:accountId', isAuth, accountsController.deleteAccount);
 
 module.exports = router;
