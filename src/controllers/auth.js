495c5a9f93c8c77dabfda38cd7ce3000a1cf6974
Renato Martins <renatto.martins@gmail.com>
2023-06-27 07:46:20 -0300

    Includes validation on Auth routes

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,16 +1,25 @@
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
+  const validationErrors = validationResult(req);
 
   try {
+    if (!validationErrors.isEmpty()) {
+      const error = new Error('Unprocessable Entity');
+      error.statusCode = 422;
+      error.details = validationErrors.array();
+      throw error;
+    }
+
     const loadedUser = await User.scope('comparePassword').findOne({ where: { email } });
 
     if (!loadedUser) {
-      const error = new Error('Unauthorized');
-      error.statusCode = 401;
+      const error = new Error('Not found');
+      error.statusCode = 404;
       throw error;
     }
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,11 +1,15 @@
 const express = require('express');
-// eslint-disable-next-line no-unused-vars
 const { body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/login', authController.login);
+router.post(
+  '/login',
+  body('email').exists().notEmpty().isEmail().withMessage('Invalid email format').normalizeEmail(),
+  body('password').exists().notEmpty(),
+  authController.login
+);
 
 module.exports = router;
