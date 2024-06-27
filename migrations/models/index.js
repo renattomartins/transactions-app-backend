14a325fb78195a24ae07b84297dba0873f1e0f0b
Renato Martins <renatto.martins@gmail.com>
2024-06-27 09:35:05 -0300

    Troubleshooting migration connection

--- migrations/models/index.js
+++ migrations/models/index.js
@@ -19,6 +19,12 @@ const db = {};
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 fs.readdirSync(__dirname)
