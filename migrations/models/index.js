71bae0cb7676c1843206da826b9a0a8400d0d9a9
Renato Martins <renatto.martins@gmail.com>
2024-06-27 09:28:05 -0300

    Debug migration proccess

--- migrations/models/index.js
+++ migrations/models/index.js
@@ -1,26 +1,25 @@
 'use strict';
 
-console.log(`${__dirname}/../config/config.js`);
-
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-console.log(env);
 const config = require(__dirname + '/../config/config.js')[env];
-console.log(config);
-console.log(JSON.stringify(config));
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+});
 
 fs.readdirSync(__dirname)
   .filter((file) => {
